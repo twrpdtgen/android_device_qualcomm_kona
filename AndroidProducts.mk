58c7dff9d80c608c1ab700fd421ea01736499cd8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-05-07 12:28:16 +0200

    kona: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: qualcomm Model: Kona for arm64 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_kona.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_kona-user \
+    omni_kona-userdebug \
+    omni_kona-eng
